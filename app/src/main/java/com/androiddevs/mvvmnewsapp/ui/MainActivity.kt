953c9e4eb022bfb0d2fe79c80c059d7d912e8963
Ihar Khorau <dostup.96@gmail.com>
2022-06-23 19:18:28 +0300

    Add Fragments and setup navigation
    Added empty fragments for future screens:
     - Breaking news
     - Search news
     - Saved news
     - Article
    
    Added base ViewBinding fragment
    Set up navigation with NavigationComponent

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+    }
+}
