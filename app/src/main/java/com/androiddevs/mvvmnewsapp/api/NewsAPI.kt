cd1eaa2124a8db1e33da53fc214fb774a598caca
Ihar Khorau <dostup.96@gmail.com>
2022-06-29 21:17:27 +0300

    Setup Room 2
    Created Database and TypeConverters

--- app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/api/NewsAPI.kt
@@ -1,6 +1,6 @@
 package com.androiddevs.mvvmnewsapp.api
 
-import com.androiddevs.mvvmnewsapp.data.NewsResponse
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
